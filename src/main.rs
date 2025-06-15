7b1469fab25e24604abb9d1ecfc941f486a29abd
Mimoalexer <MimoAlexer@gmail.com>
2025-06-15 15:59:09 +0200

    some testing with the example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use serenity::async_trait;
+use serenity::builder::CreateMessage;
+use serenity::model::channel::Message;
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, context: Context, msg: Message) {
+        if msg.content == "!messageme" {
+            // If the `utils`-feature is enabled, then model structs will have a lot of useful
+            // methods implemented, to avoid using an often otherwise bulky Context, or even much
+            // lower-level `rest` method.
+            //
+            // In this case, you can direct message a User directly by simply calling a method on
+            // its instance, with the content of the message.
+            let builder = CreateMessage::new().content("Bro ich wurde in rust geschrieben und bin der giga gooner");
+            let channel = msg.channel_id;
+            channel.send_message(&context, builder).await.unwrap();
+        }
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    // Configure the client with your Discord bot token in the environment.
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+    let mut client =
+        Client::builder(&token, intents).event_handler(Handler).await.expect("Err creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {why:?}");
+    }
 }
