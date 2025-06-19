2bbf231c3eb5a4b6eade974086e9fd44e8a9a357
Mimoalexer <MimoAlexer@gmail.com>
2025-06-19 18:58:55 +0200

    fix: update embed fields and improve error handling in command response

--- src/commands/info.rs
+++ src/commands/info.rs
@@ -1,4 +1,4 @@
-use serenity::all::{CommandInteraction, Context, CreateCommand, CreateEmbed, CreateEmbedFooter, CreateInteractionResponse, CreateInteractionResponseMessage, InteractionContext, ResolvedOption, Timestamp};
+use serenity::all::{CommandInteraction, Context, CreateCommand, CreateEmbed, CreateEmbedFooter, CreateInteractionResponse, CreateInteractionResponseMessage, Timestamp};
 
 pub async fn run(command: CommandInteraction, ctx: &Context) {
     let footer = CreateEmbedFooter::new("This is a test footer");
@@ -7,16 +7,17 @@ pub async fn run(command: CommandInteraction, ctx: &Context) {
         .description("This is an important test description, and will be used for testing purposes because of testing its an test and test its an testing")
         .image("https://avatars.githubusercontent.com/u/5430905?s=200&v=4")
         .fields(vec![
-            ("This is the first field", "This is a field body", true),
+            //Not working
+            (format!("Ping: {} ms", ctx.http.).as_str(), "my Ping is usually high because i'm a discord bot!", true),
             ("This is the second field", "Both fields are inline", true),
         ])
         .field("This is the third field", "This is not an inline field", false)
         .footer(footer)
-        // Add a timestamp for the current time
-        // This also accepts a rfc3339 Timestamp
         .timestamp(Timestamp::now());
     let response = CreateInteractionResponse::Message(CreateInteractionResponseMessage::new().embed(embed));
-    command.create_response(ctx, response).await.unwrap();
+    if let Err(e) = command.create_response(ctx, response).await {
+        println!("Failed to create response: {:?}", e);
+    }
 }
 
 pub fn register() -> CreateCommand {
