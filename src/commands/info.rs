bb09d38c85fc630714cfc6658416de1822b5d882
Mimoalexer <MimoAlexer@gmail.com>
2025-06-20 17:20:05 +0200

    fix: correct ping reference in embed fields and clean up commented code

--- src/commands/info.rs
+++ src/commands/info.rs
@@ -2,13 +2,14 @@ use serenity::all::{CommandInteraction, Context, CreateCommand, CreateEmbed, Cre
 
 pub async fn run(command: CommandInteraction, ctx: &Context) {
     let footer = CreateEmbedFooter::new("This is a test footer");
+    let ping = &ctx.http;
     let embed = CreateEmbed::new()
         .title("This is a test Title")
         .description("This is an important test description, and will be used for testing purposes because of testing its an test and test its an testing")
         .image("https://avatars.githubusercontent.com/u/5430905?s=200&v=4")
         .fields(vec![
             //Not working
-            (format!("Ping: {} ms", ctx.http.).as_str(), "my Ping is usually high because i'm a discord bot!", true),
+            (format!("Ping: {} ms", ping).as_str(), "my Ping is usually high because i'm a discord bot!", true),
             ("This is the second field", "Both fields are inline", true),
         ])
         .field("This is the third field", "This is not an inline field", false)
--- src/main.rs
+++ src/main.rs
@@ -11,56 +11,6 @@ struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    /*
-    async fn message(&self, context: Context, msg: Message) {
-        if msg.content == "!messageme" {
-            // If the `utils`-feature is enabled, then model structs will have a lot of useful
-            // methods implemented, to avoid using an often otherwise bulky Context, or even much
-            // lower-level `rest` method.
-            //
-            // In this case, you can direct message a User directly by simply calling a method on
-            // its instance, with the content of the message.
-            let builder = CreateMessage::new().content("Bro ich wurde in rust geschrieben und bin der giga gooner");
-            let channel = msg.channel_id;
-            channel.send_message(&context, builder).await.unwrap();
-        }
-    }
-    */
-
-    /*
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!hello" {
-            // The create message builder allows you to easily create embeds and messages using a
-            // builder syntax.
-            // This example will create a message that says "Hello, World!", with an embed that has
-            // a title, description, an image, three fields, and a footer.
-            let footer = CreateEmbedFooter::new("This is a footer");
-            let embed = CreateEmbed::new()
-                .title("This is a title")
-                .description("This is a description")
-                .image("attachment://ferris_eyes.png")
-                .fields(vec![
-                    ("This is the first field", "This is a field body", true),
-                    ("This is the second field", "Both fields are inline", true),
-                ])
-                .field("This is the third field", "This is not an inline field", false)
-                .footer(footer)
-                // Add a timestamp for the current time
-                // This also accepts a rfc3339 Timestamp
-                .timestamp(Timestamp::now());
-            let builder = CreateMessage::new()
-                .content("Hello, World!")
-                .embed(embed)
-                .add_file(CreateAttachment::path("./Untitled.png").await.unwrap());
-            let msg = msg.channel_id.send_message(&ctx.http, builder).await;
-
-            if let Err(why) = msg {
-                println!("Error sending message: {why:?}");
-            }
-        }
-    }
-     */
-
     async fn ready(&self, ctx: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
 
